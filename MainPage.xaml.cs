d9d0530bc5c11316ddb543a77cc4de16c9e3046d
vegatona <oswaldovega756@gmail.com>
2025-02-25 11:35:25 -0700

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiCalculadora_JOVT
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            var altura = double.Parse(Altura.Text);
+            var peso = double.Parse(Peso.Text);
+            var imc = peso / (altura * altura);
+
+            var Resultado = imc;
+
+            IMC.Text = Resultado.ToString();
+        }
+    }
+
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiCalculadora_JOVT
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
